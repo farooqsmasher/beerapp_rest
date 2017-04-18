430152130b2fc06963c69c44319c8d6e6e027709
iamviqar <iamviqar@gmail.com>
2017-04-18 23:13:41 +0530

    Revert "Revert "swagger and user existing check""
    This reverts commit 0e073ea00037a7ee385ecc1ff77518a228bbf156.

--- src/main/java/com/webteam_rest/dao/UserCredDAO.java
+++ src/main/java/com/webteam_rest/dao/UserCredDAO.java
@@ -11,4 +11,5 @@ public interface UserCredDAO {
 	List<UserCred> getAllUserCred() throws DataServiceException;
 	UserCred getUserCredById(Long id) throws DataServiceException;
 	UserCred getUserByUsernamePassword(String userName, String password) throws DataServiceException;
+	UserCred getUserByUsername(String userName) throws DataServiceException;
 }
--- src/main/java/com/webteam_rest/services/UserCredService.java
+++ src/main/java/com/webteam_rest/services/UserCredService.java
@@ -13,4 +13,5 @@ public interface UserCredService {
 	UserCred doGetUserCredById(Long id) throws BusinessServiceException;
 	void doEnableUserCredById(Long id) throws BusinessServiceException;
 	UserCred doGetUserByUsernamePassword(String userName, String password) throws BusinessServiceException;
+	UserCred doGetUserByUsername(String userName) throws BusinessServiceException;
 }
